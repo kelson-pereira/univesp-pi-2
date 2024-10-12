d77cac580e72d743d2a5456231767649ed9fd0c2
kelson-pereira <kelson.op@gmail.com>
2024-10-12 16:49:45 -0300

    Correção do comprimento do do campo descrição do formulário de registro

--- combate_aedes/forms.py
+++ combate_aedes/forms.py
@@ -13,7 +13,7 @@ class ValidarTelefone(forms.Form):
     telefone = forms.CharField(initial='', widget=forms.TextInput(attrs={'class':'form-control fs-2 mt-3','inputmode':'numeric'}), label='Informe seu número de telefone com DDD no formato 12987654321. Ele será solicitado se você optar por apagar esses dados.', validators=[RegexValidator('^([0-9]{11})$', message='Telefone inválido, use somente números.')])
 
 class ValidarDescricao(forms.Form):
-    descricao = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control fs-4 mt-3','rows': '3'}), label='(Opcional) Se possível, forneça uma descrição do local suspeito, como água parada em latas, garrafas vazias, pneus, lixo, entre outros.', required=False)
+    descricao = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control fs-4 mt-3','rows': '3', 'maxlength': '128'}), label='(Opcional) Se possível, forneça uma descrição do local suspeito, como água parada em latas, garrafas vazias, pneus, lixo, entre outros.', required=False)
 
 class ValidarPolitica(forms.Form):
     termos = forms.BooleanField(initial=False, label='Concordo com a política.', widget=forms.CheckboxInput(attrs={'class':'form-check-input'}), required=True)
